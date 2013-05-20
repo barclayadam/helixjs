95f3f5433d54ffad5c3d1b7c290051b3833350ca
Adam Barclay <barclay.adam@gmail.com>
2013-05-20 17:16:04 +0100

    Use a single event handler for all nav links
    For every link an event handler was being registered, now a single
    document-scoped handler is used.

--- src/ui/ui.navigate.js
+++ src/ui/ui.navigate.js
@@ -1,5 +1,22 @@
 hx.config(['$router'], function($router) {
 
+    ko.utils.registerEventHandler(document, 'click', function(event) {
+        if(event.target.tagName === 'A') {
+            var element = event.target,
+                routeName = ko.utils.domData.get(element, '__routeName'),
+                parameters = ko.utils.domData.get(element, '__routeParameters');
+
+            if(routeName) {
+                $router.navigateTo(routeName, parameters);
+
+                if (event.preventDefault)
+                    event.preventDefault();
+                else
+                    event.returnValue = false;
+            }
+        }
+    });
+
     /**
      * @bindingHandler navigate
      *
@@ -21,7 +38,7 @@ hx.config(['$router'], function($router) {
      * <a data-bind="navigate: 'View User', parameters: { userId: 4 }>View User 4</a>"
      */
     koBindingHandlers.navigate = {
-        init: function(element, valueAccessor, allBindingsAccessor) {
+        /*init: function(element, valueAccessor, allBindingsAccessor) {
             var routeName = ko.utils.unwrapObservable(valueAccessor()),
                 parameters = allBindingsAccessor()['parameters'];
 
@@ -33,7 +50,7 @@ hx.config(['$router'], function($router) {
                 else
                     event.returnValue = false;
             })
-        },
+        },*/
 
         update: function(element, valueAccessor, allBindingsAccessor) {
             var routeName = ko.utils.unwrapObservable(valueAccessor()),
@@ -41,6 +58,9 @@ hx.config(['$router'], function($router) {
                 url = $router.buildUrl(routeName, parameters);
 
             element.setAttribute('href', url || '#');
+
+            ko.utils.domData.set(element, '__routeName', routeName);
+            ko.utils.domData.set(element, '__routeParameters', parameters);
         }
     }
 });
