8452e41915043893cad63607b663fff5bcb54999
Adam Barclay <barclay.adam@gmail.com>
2015-06-11 10:00:59 +0100

    [Storage] More robust feature detection
    In Safari privacy mode a DOM error 18 can be thrown if in private
    browsing mode and localStorage property is touched (even without calling
    methods)

--- src/core/storage.js
+++ src/core/storage.js
@@ -1,33 +1,36 @@
 (function() {
-    var localStorageSupported = window['localStorage'] !== undefined,
-        testKey = 'qeTest', 
-        storage = window.localStorage; 
+    // Test taken from Modernizr: https://github.com/Modernizr/Modernizr/blob/master/feature-detects/storage/localstorage.js
+    function isLocalStorageSupported() {
+        var mod = 'hx';
 
-    // We need to test whether we could actually use localStorage. In Safari in private browsing
-    // mode (local|window)Storage is available, but will throw a storage exception on any attempt at
-    // using it.
-    if (localStorageSupported) {
-        try { 
-            // Try and catch quota exceeded errors 
-            window['localStorage'].setItem(testKey, '1'); 
-            storage.removeItem(testKey); 
-        } catch (error) { 
-            if (typeof DOMException === 'undefined' || error.code === DOMException.QUOTA_EXCEEDED_ERR && storage.length === 0) {
-                localStorageSupported = false;
-            }
+        try {
+            localStorage.setItem(mod, mod);
+            localStorage.removeItem(mod);
+            return true;
+        } catch (e) {
+            return false;
         }
     }
 
-    // Creates observable extenders for both `local` and `session` storage.
+    if (!isLocalStorageSupported()) {
+        "local session".replace(/\w+/g, function (type) {
+            ko.extenders[type + 'Storage'] = function (target, key) {
+                // No-op required to maintain the same interface as supported scenario.
+                target.store = function() {};
 
-    // The storage extenders handle the conversion from and to a string for
-    // any data type stored (including object literals), as the underlying
-    // storage mechanism (as defined by the `HTML5` spec) does not perform
-    // storage of any 'complex' data type.
-    "local session".replace(/\w+/g, function (type) {
-        ko.extenders[type + 'Storage'] = function (target, key) {
-            function store(newValue) {
-                if (localStorageSupported) {
+                return target;
+            };
+        });
+    } else {
+        // Creates observable extenders for both `local` and `session` storage.
+
+        // The storage extenders handle the conversion from and to a string for
+        // any data type stored (including object literals), as the underlying
+        // storage mechanism (as defined by the `HTML5` spec) does not perform
+        // storage of any 'complex' data type.
+        "local session".replace(/\w+/g, function (type) {
+            ko.extenders[type + 'Storage'] = function (target, key) {
+                function store(newValue) {
                     if (newValue !== undefined) {
                         window[type + 'Storage'].setItem(key, JSON.stringify({
                             value: newValue
@@ -36,28 +39,26 @@
                         window[type + 'Storage'].removeItem(key);
                     }
                 }
-            }
 
-            if (localStorageSupported) {
                 var stored = window[type + 'Storage'].getItem(key);
                 
                 if (stored != null && ko.isWriteableObservable(target)) {
                     target((JSON.parse(stored)).value);
                 }
-            }
 
-            // Automatically store new value to storage (will only work if
-            // support is enabled)
-            target.subscribe(store);
+                // Automatically store new value to storage (will only work if
+                // support is enabled)
+                target.subscribe(store);
 
-            // Force the current value to be stored, useful for when the value
-            // is initialised but not changed and you want value to be exposed
-            // in DevTools
-            target.store = function() {
-                store(target());
+                // Force the current value to be stored, useful for when the value
+                // is initialised but not changed and you want value to be exposed
+                // in DevTools
+                target.store = function() {
+                    store(target());
+                };
+
+                return target;
             };
-
-            return target;
-        };
-    });
+        });
+    }
 }());
